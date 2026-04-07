70e3f5fc76a9be547596b6a6917e8303914eae39
ambarcacciola <acacciola@mail.austral.edu.ar>
2026-04-07 10:58:15 -0300

    Update exercise_concatenate_lists.py

--- exercise_concatenate_lists.py
+++ exercise_concatenate_lists.py
@@ -1,14 +1,5 @@
 # Ejercicio 10: Concatenar listas
 
 def concatenate_lists(lista1, lista2):
-    """
-    Concatena dos listas en una sola.
-
-    Args:
-        lista1: Primera lista
-        lista2: Segunda lista
-
-    Returns:
-        Una nueva lista con todos los elementos de lista1 seguidos de lista2
-    """
-    pass  # Reemplazar con tu implementación
+    nueva_lista = lista1 + lista2
+    return nueva_lista
